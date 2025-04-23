5d4625be2a454a4edbd39137922f552f71ec35d3
aashidutt <aashi.dutt3@gmail.com>
2025-04-23 12:46:08 +0530

    Clean up

--- statistics_helper.py
+++ statistics_helper.py
@@ -1,36 +1,3 @@
-# from scipy.stats import ttest_ind, sem, t
-# import numpy as np
-
-# def recalculate_p_value(group1, group2):
-#     t_stat, p_value = ttest_ind(group1, group2, equal_var=False)
-#     return {"p_value": round(p_value, 4)}
-
-# def compute_cohens_d(group1, group2):
-#     mean1, mean2 = np.mean(group1), np.mean(group2)
-#     std1, std2 = np.std(group1, ddof=1), np.std(group2, ddof=1)
-#     pooled_std = np.sqrt((std1**2 + std2**2) / 2)
-#     d = (mean1 - mean2) / pooled_std
-#     return {"cohens_d": round(d, 4)}
-
-# def compute_confidence_interval(data, confidence=0.95):
-#     data = np.array(data)
-#     n = len(data)
-#     mean = np.mean(data)
-#     margin = sem(data) * t.ppf((1 + confidence) / 2., n-1)
-#     return {
-#         "mean": round(mean, 4),
-#         "confidence_interval": [round(mean - margin, 4), round(mean + margin, 4)],
-#         "confidence": confidence
-#     }
-
-# def describe_group(data):
-#     data = np.array(data)
-#     return {
-#         "mean": round(np.mean(data), 4),
-#         "std_dev": round(np.std(data, ddof=1), 4),
-#         "n": len(data)
-#     }
-
 from scipy.stats import ttest_ind, sem, t
 import numpy as np
 
